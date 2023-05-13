bad83b72682202a053d6912eb15fdd412aa80ee9
codernoyan <coder.noyan@gmail.com>
2023-05-13 23:47:25 +0600

    added coffee animation as loading

--- src/ui/Loading.jsx
+++ src/ui/Loading.jsx
@@ -1,7 +1,11 @@
+import Lottie from "lottie-react";
+import coffeeLoading from 'assets/coffee-loading.json';
+
 export default function Loading() {
   return (
     <section className="flex justify-center items-center h-screen">
-      <h2>Loading...</h2>
+      {/* lottie */}
+      <Lottie className="aspect-video" animationData={coffeeLoading} loop={true} />
     </section>
   )
 }
